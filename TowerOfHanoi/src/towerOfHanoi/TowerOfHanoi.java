0107004c8962624a643bde793e1dd82d9ddcf6d3
GjermundStrøm <gjermundstroem@outlook.com>
2016-04-20 15:09:35 +0200

    v0.2 Programmet teller hvor mange flyttinger som er gjort

--- TowerOfHanoi/src/towerOfHanoi/TowerOfHanoi.java
+++ TowerOfHanoi/src/towerOfHanoi/TowerOfHanoi.java
@@ -1,8 +1,7 @@
 package towerOfHanoi;
 
 public class TowerOfHanoi {
-	
-
+	private int teller = 0;
 	
 	public void flyttDisker(int topN, char fra, char inter, char til) {
 		if (topN == 1)
@@ -16,12 +15,17 @@ public class TowerOfHanoi {
 	}
 	
 	public void print(String s){
+		teller++;
 		System.out.println(s);		
+	}
+	public int getTeller(){
+		return this.teller;
 	}
 	
 	public static void main(String[] args) {
 		TowerOfHanoi tower = new TowerOfHanoi();
 		int nDisks = 3;
 		tower.flyttDisker(nDisks, 'A', 'B', 'C');
+		System.out.println("Det ble gjort " +tower.getTeller() + " flyttinger.");
 	}
 }
